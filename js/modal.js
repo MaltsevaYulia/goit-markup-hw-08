d939da010fb9f4921a4e08365b4499436e5f50cf
MaltsevaYulia <112495890+MaltsevaYulia@users.noreply.github.com>
2023-01-07 20:31:10 +0200

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -4,13 +4,21 @@
       closeModalBtn: document.querySelector("[data-modal-close]"),
       modal: document.querySelector("[data-modal]"),
       body: document.querySelector('body'),
+      backdrop: document.querySelector('.backdrop')
     };
   
     refs.openModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
+    refs.backdrop.addEventListener("click", onBackdropClick);
   
     function toggleModal() {
       refs.modal.classList.toggle("is-hidden");
       refs.body.classList.toggle('no-scroll');
-    }
+      
+  }
+  function onBackdropClick(event) {
+  if (event.currentTarget === event.target) {
+    toggleModal();
+  }
+}
   })();
